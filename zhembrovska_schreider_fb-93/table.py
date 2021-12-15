031076063abd7fef85ba159d7bf0ca627b2e0117
OlenaZhembrovska <88781966+OlenaZhembrovska@users.noreply.github.com>
2021-12-15 14:06:23 +0200

    added example table & modify some enters

--- zhembrovska_schreider_fb-93/table.py
+++ zhembrovska_schreider_fb-93/table.py
@@ -1,13 +1,13 @@
 #from main import 
 
 class Table:
+    name = ''
     titles = []
     elements = []
 
-
-
-    def separation(n):
-        print("-" * n)
+    def print_name(table_name):
+        name = table_name
+        print(table_name)
 
     def print_dblarr(mas):
         for x in range(0, len(mas)): 
@@ -16,37 +16,33 @@ class Table:
             print( ' |') 
 
     def add_new_title(titles, new_title):
-       
-        #for i in range(number_of_lines):
-        #elem_mas.append(list(map(str , input("Enter elements: ").split(','))))
-        #new_title = input("Print new title:  ")
         titles.append(new_title)
 
     def delete_title(arr):
-                delete_index = int(input("Which title do you want to delete? \n"))
-                del arr[delete_index]
+        delete_index = int(input("Which title do you want to delete? \n"))
+        arr[delete_index] = '____'
 
 
 
     def print_table(self, titles_arr, elements_mas):
         print("|    "+('    |    '.join(titles_arr)) + '    |')
-        #separation(40)
-        print("-" * 40)
+        #self.separation(40)
+        print("-" * 60)
         self.print_dblarr(elements_mas)
         
-    def add_elem_line(elements_mas):
-        #for i in range(number_of_lines):
-        elements_mas.append(list(map(str , input("Enter elements: ").split(','))))
+    def add_elem_line(elements_mas, line):
+        print(line)
+        elements_mas.append(list(map(str , line.split(','))))
 
 
     def delete_elem_line(elements_mas):
         delete_line = int(input("Which line do you want to delete? \n"))
         del elements_mas[delete_line]
 
-    def change_element(elements_mas):
+    def change_element(elements_mas, element):
         line = int(input("Which line is the element you want to delete? \n"))
         index= int(input("Which index have the element you want to delete? \n"))
-        elements_mas[line][index] = input('Enter new element:  ')
+        elements_mas[line][index] = element
 
     def delete_element(elements_mas):
         delete_line = int(input("Which line is the element you want to delete? \n"))
